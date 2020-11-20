949db3c7afde31dc920cd75828a26c01b66a28c8
lukehayes <lukepurplehayes@gmail.com>
2020-11-20 12:52:20 +0000

    Added Symfony\Request and Symfony\Response logic to index.php

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+require_once __DIR__.'/vendor/autoload.php';
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+
+$request = Request::createFromGlobals();
+
+$name = $request->get('name', 'World');
+
+$response = new Response( sprintf("Hello %s", htmlspecialchars($name, ENT_QUOTES, 'UTF-8') ));
+$response->headers->set('Content-Type', 'text/ruby');
+
+echo $response;
+
+$response->send();
