48de45686074533a59bc39bbbed3e7121a57f7a5
lukehayes <lukepurplehayes@gmail.com>
2020-11-20 17:45:03 +0000

    Added new template files

---
+++ front.php
@@ -0,0 +1,26 @@
+<?php
+require_once __DIR__.'/init.php';
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+$request = Request::createFromGlobals();
+$request = Request::create('/hello?name=Luke');
+$response = new Response();
+
+$map = [
+    '/hello' => 'hello.php',
+    '/bye' => 'bye.php',
+];
+
+$path = $request->getPathInfo();
+
+if(isset($map[$path])) {
+    require $map[$path];
+}else
+{
+    $response->setStatusCode(404);
+    $response->setContent("$map[$path] Not Found.");
+}
+
+$response->send();
