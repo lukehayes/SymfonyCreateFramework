2b4a9d758a5cdd1089e690eb146442673968b7cd
lukehayes <lukepurplehayes@gmail.com>
2020-11-23 18:20:23 +0000

    Updated tutorial additions

--- index.php
+++ index.php
@@ -1,17 +1,25 @@
 <?php
-require_once __DIR__.'/vendor/autoload.php';
+require_once __DIR__.'/init.php';
 
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
-
 $request = Request::createFromGlobals();
+$response = new Response();
 
-$name = $request->get('name', 'World');
+$map = [
+    '/hello' => 'hello.php',
+    '/bye' => 'bye.php',
+];
 
-$response = new Response( sprintf("Hello %s", htmlspecialchars($name, ENT_QUOTES, 'UTF-8') ));
-$response->headers->set('Content-Type', 'text/ruby');
+$path = $request->getPathInfo();
 
-echo $response;
+if(isset($map[$path])) {
+    require $map[$path];
+}else
+{
+    $response->setStatusCode(404);
+    $response->setContent("$map[$path] Not Found.");
+}
 
 $response->send();
