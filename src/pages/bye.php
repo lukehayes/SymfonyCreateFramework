c1ce59f950a87040a3eb41b80f50eb01956e23ab
lukehayes <lukepurplehayes@gmail.com>
2020-11-24 11:31:54 +0000

    Removed require line from templates as its not needed

--- src/pages/bye.php
+++ src/pages/bye.php
@@ -1,4 +1,2 @@
 <?php
-require_once __DIR__.'/init.php';
-
 $response->setContent('Goodbye');
--- src/pages/hello.php
+++ src/pages/hello.php
@@ -1,5 +1,3 @@
 <?php
-require_once __DIR__.'/init.php';
-
 $name = $request->get('name', 'World');
 $response->setContent(sprintf('Hello %s', htmlspecialchars($name, ENT_QUOTES, 'UTF-8')));
