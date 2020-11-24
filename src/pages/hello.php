beded1e6ad755b5f7bd01fd1eeada83f6d73970e
lukehayes <lukepurplehayes@gmail.com>
2020-11-24 17:30:25 +0000

    Simplified hello template

--- src/pages/hello.php
+++ src/pages/hello.php
@@ -1,3 +1,2 @@
-<?php $name = $request->get('name', 'World'); ?>
-Hello <?= htmlspecialchars($name, ENT_QUOTES, "UTF-8") ?>
+Hello <?= htmlspecialchars(isset($name) ? $name : " World", ENT_QUOTES, "UTF-8") ?>
 
