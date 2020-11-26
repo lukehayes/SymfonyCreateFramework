46ff623da2cb7da74acaccd9e31ffa710814b636
lukehayes <lukepurplehayes@gmail.com>
2020-11-26 17:57:41 +0000

    Removed home root

--- src/app.php
+++ src/app.php
@@ -5,7 +5,6 @@ use Symfony\Component\Routing\Route;
 
 $routes = new RouteCollection();
 
-$routes->add('home', new Route('/'), [] );
 $routes->add('hello', new Route('/hello/{name}'), ['name' => 'World'] );
 $routes->add('bye', new Route('/bye'));
 
