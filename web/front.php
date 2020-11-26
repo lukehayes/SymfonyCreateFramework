446ef0ba2fd589dde0e18ccf099415c5bffa84c1
lukehayes <lukepurplehayes@gmail.com>
2020-11-26 14:51:39 +0000

    Added Routing Component classes

--- web/front.php
+++ web/front.php
@@ -4,18 +4,25 @@ require_once __DIR__.'/../vendor/autoload.php';
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\RouteCollection;
+use Symfony\Component\Routing\Matcher\UrlMatcher;
+use Symfony\Component\Routing\RequestContext;
+use Symfony\Component\Routing\Route;
 
 $request = Request::createFromGlobals();
 
-
 $routes = new RouteCollection();
+$routes->add('home', new Route('/'), [] );
+$routes->add('hello', new Route('/hello/{name}'), ['name' => 'World'] );
+$routes->add('bye', new Route('/bye'));
 
-//$map = [
-    //'/hello' => 'hello',
-    //'/bye' => 'bye',
-//];
+$context = new RequestContext();
+$context->fromRequest($request);
+$matcher = new UrlMatcher($routes, $context);
 
-$path = $request->getPathInfo();
+$attributes = $matcher->match($request->getPathInfo());
+
+var_dump($attributes);
+
 
 if(isset($map[$path])) {
     ob_start();
