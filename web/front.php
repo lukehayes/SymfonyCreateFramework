2299cbc9d305f121f684a6669ecf89484b633c17
lukehayes <lukepurplehayes@gmail.com>
2020-11-26 17:57:55 +0000

    Basic routing solution working now

--- web/front.php
+++ web/front.php
@@ -3,20 +3,34 @@ require_once __DIR__.'/../vendor/autoload.php';
 
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\RouteCollection;
 use Symfony\Component\Routing\Matcher\UrlMatcher;
 use Symfony\Component\Routing\RequestContext;
-use Symfony\Component\Routing\Route;
+use Symfony\Component\Routing\Exception\ResourceNotFoundException;
 
 $request = Request::createFromGlobals();
-$routes = include __DIR__ . "../src/app.php";
+$routes = include __DIR__ . "/../src/app.php";
 
 $context = new RequestContext();
 $context->fromRequest($request);
-$matcher = new UrlMatcher($routes, $context);
 
-$attributes = $matcher->match($request->getPathInfo());
+try {
 
+    $matcher = new UrlMatcher($routes, $context);
+    extract( $matcher->match($request->getPathInfo()) , EXTR_SKIP);
+    ob_start();
+    include sprintf(__DIR__ . "/../src/pages/%s.php", $_route);
+
+    $response = new Response(ob_get_clean());
+
+} catch(ResourceNotFoundException $e) {
+    $response = new Response("Not Found", 404);
+
+} catch(Exception $e) {
+    $response = new Response("An Error Occurred", 500);
+}
+
+
+$response->send();
 
 
 
