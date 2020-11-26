d46776f2bc7d5a165aa4b4830d94e07faa104ea2
lukehayes <lukepurplehayes@gmail.com>
2020-11-26 16:01:04 +0000

    Removed debug dump() calls

--- web/front.php
+++ web/front.php
@@ -17,18 +17,6 @@ $matcher = new UrlMatcher($routes, $context);
 
 $attributes = $matcher->match($request->getPathInfo());
 
-dump($attributes);
-dump($matcher->match('/byea'));
 
-//if(isset($map[$path])) {
-    //ob_start();
-    //extract($request->query->all(), EXTR_SKIP);
-    //include sprintf(__DIR__ . '/../src/pages/%s.php', $map[$path]);
-    //$response = new Response(ob_get_clean());
 
-//}else
-//{
-    //$response = new Response("Not Found", 404);
-//}
 
-//$response->send();
